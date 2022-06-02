106c9d4a1b4a99cbc488aa56b3a908ba399180eb
leosouzv <leogrdss@icloud.com>
2022-06-02 15:49:00 -0300

    Model and service created

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/User';
+import { Userlogin } from '../model/UserLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  entrar(userLogin: Userlogin): Observable<Userlogin>{
+    return this.http.post<Userlogin>('http://localhost:8080/usuarios/logar', userLogin)
+  }
+
+  cadastrar(user: User):Observable<User>{
+    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar' ,user)
+
+  }
+}
